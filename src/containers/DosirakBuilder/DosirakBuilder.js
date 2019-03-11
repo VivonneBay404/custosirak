1503a7ee684da86362bc83081ec461d5cedfc88a
Ji Hong Park <asthma404@gmail.com>
2019-03-11 12:50:26 +0900

    menuClickHandler-2

--- src/containers/DosirakBuilder/DosirakBuilder.js
+++ src/containers/DosirakBuilder/DosirakBuilder.js
@@ -7,19 +7,19 @@ class DosirakBuilder extends Component {
     state = {
         menu: {
             반찬1: {
-                제육볶음 :{
-                    selected: false, price : 1000 
+                제육볶음: {
+                    selected: false, price: 1000
                 },
                 불고기: {
-                    selected: false , price: 1200
+                    selected: false, price: 1200
                 }
             },
             반찬2: {
                 오뎅볶음: {
-                    selected: false, price : 800 
+                    selected: false, price: 800
                 },
                 가지볶음: {
-                    selected: false, price : 900
+                    selected: false, price: 900
                 }
             }
         },
@@ -29,29 +29,39 @@ class DosirakBuilder extends Component {
         const oldSection = this.state.menu[section];
         const oldItem = oldSection[item];
         const oldItemSelected = oldItem.selected;
-        const newItemSelected = !oldItemSelected;
 
-        const updatedMenu = {...this.state.menu};
-        const updatedSection =updatedMenu[section];
+
+        const updatedMenu = { ...this.state.menu };
+        const updatedSection = updatedMenu[section];
         const updatedItem = updatedSection[item];
-        updatedItem.selected = newItemSelected;
+        let updatedItemSelected = null;
 
-        const oldCount = this.state.totalPrice;
-        let newCount = 0;
-
-        if (newItemSelected) {
-            newCount = oldCount + oldItem.price
+        if (oldItemSelected) {
+            updatedItemSelected = !oldItemSelected;
         } else {
-            newCount = oldCount - oldItem.price;
+            for(const items in oldSection){
+                oldSection[items].selected = false
+            }
+            updatedItemSelected = !oldItemSelected;
         }
-        this.setState({totalPrice: newCount, menu:updatedMenu })
+        updatedItem.selected = updatedItemSelected;
+        let totalPrice = 0
+        for(const updatedSection in updatedMenu){
+            for(const item in updatedMenu[updatedSection]){
+                if(updatedMenu[updatedSection][item].selected === true){
+                    totalPrice = totalPrice + updatedMenu[updatedSection][item].price
+                }
+            }
+        }
+        this.setState({ menu: updatedMenu, totalPrice: totalPrice})
+        console.log(this.state)
     }
 
     render() {
         return (
             <div className={classes.DosirakBuilder}>
                 <h1>DosirakBuilder Display</h1>
-                <BuildControls clicked={this.menuClickHandler} menu = {{...this.state.menu}}/>
+                <BuildControls clicked={this.menuClickHandler} menu={{ ...this.state.menu }} />
                 <p>총 가격 : {this.state.totalPrice}</p>
             </div>
 
